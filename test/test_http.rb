b86d4691245ac58707904937d1dc8d3a23247ae5
hukl <contact@smyck.org>
2010-08-14 17:19:19 +0200

    basic tests and method missing for get, post, put and delete

--- test/test_http.rb
+++ test/test_http.rb
@@ -2,95 +2,127 @@ require 'helper'
 
 class TestHttp < Test::Unit::TestCase
 
-  test "create the simplest http get object" do
-    assert_not_nil get = HTTP.new( {:host => "localhost"} )
+  test "cannot create HTTP object without any parameters" do
+    assert_raise( ArgumentError) { HTTP.new }
   end
 
-  test "to create a http object at least the host must be specified" do
-    assert_raise(ArgumentError) { get = HTTP.new( {} ) }
+  test "request can be built with one argument" do
+    assert_not_nil HTTP.new( "http://foobar.com" )
   end
 
-  test "http objects have accessible socket object" do
-    get = HTTP.new( {:host => "localhost"} )
-    assert_not_nil get.tcp_socket
+  test "request can be built with two or more arguments" do
+    assert_not_nil HTTP.new( "http://fooobar.com", :params => { 1 => 2} )
   end
 
-  test "http object has accessible params" do
-    get = HTTP.new( {:host => "localhost", :params => {"foo" => "bar"}} )
-    assert_not_nil get.params
-    assert_equal ({"foo" => "bar"}), get.params
+  test "method get" do
+    assert_not_nil request = HTTP.get( "http://foobar.com" )
   end
 
-  test "http object without params specified returns empty params" do
-    get = HTTP.new( {:host => "localhost"} )
-    assert_not_nil get.params
-    assert_equal ({}), get.params
+  test "method post" do
+    assert_not_nil request = HTTP.post( "http://foobar.com" )
   end
 
-  test "simple http object defaults to method GET" do
-    get = HTTP.new( {:host => "localhost"} )
-    assert_equal "GET", get.method
+  test "method put" do
+    assert_not_nil request = HTTP.put( "http://foobar.com" )
   end
 
-  test "method of http object can be overridden" do
-    post = HTTP.new( {:host => "localhost", :method => "POST"} )
-    assert_equal "POST", post.method
+  test "method delete" do
+    assert_not_nil request = HTTP.delete( "http://foobar.com" )
   end
 
-  test "path of a http object defaults to index" do
-    get = HTTP.new( {:host => "localhost"} )
-    assert_equal "/", get.path
+  test "other missing methods are delegated to super" do
+    assert_raise( ArgumentError ) { HTTP.foobar( "http://foobar.com" ) }
   end
 
-  test "path of http object can be set" do
-    get = HTTP.new( {:host => "localhost", :path => "/posts"} )
-    assert_equal "/posts", get.path
-  end
+  #test "create the simplest http get object" do
+  #  assert_not_nil get = HTTP.new( {:host => "localhost"} )
+  #end
 
-  test "generate_header_and_body" do
-    get = HTTP.new( {:host => "localhost", :path => "/posts"} )
-    assert_not_nil get.generate_header_and_body
+  #test "to create a http object at least the host must be specified" do
+  #  assert_raise(ArgumentError) { get = HTTP.new( {} ) }
+  #end
 
-    expected = "GET /posts HTTP/1.1\r\n"      \
-               "Host: localhost\r\n"          \
-               "Origin: localhost\r\n"        \
-               "Content-Length: 0\r\n"        \
-               "Content-Type: text/plain\r\n" \
-               "Connection: close\r\n\r\n"
+  #test "http objects have accessible socket object" do
+  #  get = HTTP.new( {:host => "localhost"} )
+  #  assert_not_nil get.tcp_socket
+  #end
 
-    assert_equal expected, get.header
-  end
+  #test "http object has accessible params" do
+  #  get = HTTP.new( {:host => "localhost", :params => {"foo" => "bar"}} )
+  #  assert_not_nil get.params
+  #  assert_equal ({"foo" => "bar"}), get.params
+  #end
 
-  test "multipart body gets properly created" do
-    post = HTTP.new(
-      :host   => "localhost",
-      :path   => "/photos",
-      :method => "POST",
-      :params => {
-        "photo[title]"    => "Hello World",
-        "photo[image]"    => File.open("/Users/hukl/Desktop/file1.png"),
-        "photo[picture]"  => File.open("/Users/hukl/Desktop/file2.png")
-      }
-    )
-    post.generate_header_and_body
+  #test "http object without params specified returns empty params" do
+  #  get = HTTP.new( {:host => "localhost"} )
+  #  assert_not_nil get.params
+  #  assert_equal ({}), get.params
+  #end
 
-    post.tcp_socket.write (post.header + post.body)
-    response = post.tcp_socket.recvfrom(2**16)
-    status = response.first.match(/HTTP\/1\.1\s(\d\d\d).+$/)[1]
+  #test "simple http object defaults to method GET" do
+  #  get = HTTP.new( {:host => "localhost"} )
+  #  assert_equal "GET", get.method
+  #end
 
-    assert_equal 302, status.to_i
-  end
+  #test "method of http object can be overridden" do
+  #  post = HTTP.new( {:host => "localhost", :method => "POST"} )
+  #  assert_equal "POST", post.method
+  #end
 
-  test "real get request" do
-    get = HTTP.new(
-      :host => "localhost",
-      :path => "/photos"
-    )
+  #test "path of a http object defaults to index" do
+  #  get = HTTP.new( {:host => "localhost"} )
+  #  assert_equal "/", get.path
+  #end
 
-    get.tcp_socket.write (get.header + get.body)
-    response = get.tcp_socket.recvfrom(2**16)
-    status = response.first.match(/HTTP\/1\.1\s(\d\d\d).+$/)[1]
+  #test "path of http object can be set" do
+  #  get = HTTP.new( {:host => "localhost", :path => "/posts"} )
+  #  assert_equal "/posts", get.path
+  #end
 
-    assert_equal 200, status.to_i
-  end
+  #test "generate_header_and_body" do
+  #  get = HTTP.new( {:host => "localhost", :path => "/posts"} )
+  #  assert_not_nil get.generate_header_and_body
+
+  #  expected = "GET /posts HTTP/1.1\r\n"      \
+  #             "Host: localhost\r\n"          \
+  #             "Origin: localhost\r\n"        \
+  #             "Content-Length: 0\r\n"        \
+  #             "Content-Type: text/plain\r\n" \
+  #             "Connection: close\r\n\r\n"
+
+  #  assert_equal expected, get.header
+  #end
+
+  #test "multipart body gets properly created" do
+  #  post = HTTP.new(
+  #    :host   => "localhost",
+  #    :path   => "/photos",
+  #    :method => "POST",
+  #    :params => {
+  #      "photo[title]"    => "Hello World",
+  #      "photo[image]"    => File.open("/Users/hukl/Desktop/file1.png"),
+  #      "photo[picture]"  => File.open("/Users/hukl/Desktop/file2.png")
+  #    }
+  #  )
+  #  post.generate_header_and_body
+
+  #  post.tcp_socket.write (post.header + post.body)
+  #  response = post.tcp_socket.recvfrom(2**16)
+  #  status = response.first.match(/HTTP\/1\.1\s(\d\d\d).+$/)[1]
+
+  #  assert_equal 302, status.to_i
+  #end
+
+  #test "real get request" do
+  #  get = HTTP.new(
+  #    :host => "localhost",
+  #    :path => "/photos"
+  #  )
+
+  #  get.tcp_socket.write (get.header + get.body)
+  #  response = get.tcp_socket.recvfrom(2**16)
+  #  status = response.first.match(/HTTP\/1\.1\s(\d\d\d).+$/)[1]
+
+  #  assert_equal 200, status.to_i
+  #end
 end
